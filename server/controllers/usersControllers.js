f5f7e65fd9ea416acc2da57b2350fb3be50e2acd
7lucckyy <7lucckyy6611@gmail.com>
2022-06-28 18:23:13 +0100

    changes

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -7,35 +7,35 @@ const pool = mysql.createPool({
   host:'localhost'
 })
 
-exports.view = (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) throw err
-    console.log(`connected ${connection.threadId}`);
-    connection.query('SELECT  * FROM user', (err, rows) => {
-      connection.release();
-      if (!err){
-         res.render('home',{rows}) 
-      } else { console.log(err); }
-      console.log('the data from user table: \n',rows);
-    } )
- })  
-}
-exports.find = (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) throw err
-    console.log(`connected ${connection.threadId}`);
+// exports.view = (req, res) => {
+//   pool.getConnection((err, connection) => {
+//     if (err) throw err
+//     console.log(`connected ${connection.threadId}`);
+//     connection.query('SELECT  * FROM user', (err, rows) => {
+//       connection.release();
+//       if (!err){
+//          res.render('home',{rows}) 
+//       } else { console.log(err); }
+//       console.log('the data from user table: \n',rows);
+//     } )
+//  })  
+// }
+// exports.find = (req, res) => {
+//   pool.getConnection((err, connection) => {
+//     if (err) throw err
+//     console.log(`connected ${connection.threadId}`);
 
-    let searchTerm = req.body.search;
+//     let searchTerm = req.body.search;
 
-    connection.query('SELECT * FROM user WHERE firstName LIKE ?', ['%'+ searchTerm+ '%'], (err, rows) => {
-      connection.release();
-      if (!err) {
-        res.render('home', {rows})
-      }else{console.log(err);}
-    })
+//     connection.query('SELECT * FROM user WHERE firstName LIKE ?', ['%'+ searchTerm+ '%'], (err, rows) => {
+//       connection.release();
+//       if (!err) {
+//         res.render('home', {rows})
+//       }else{console.log(err);}
+//     })
 
-  })
-}
+//   })
+// }
 
 exports.form = (req, res) => {
   res.render('add-user')
@@ -62,45 +62,45 @@ exports.contact = (req, res) => {
   res.render('contact')
 }
 
-exports.create = (req, res) => {
-  //res.render('add-user')
-const {firstName, lastName, email,phone} = req.body
-  pool.getConnection((err, connection) => {
-    if (err) throw err
-    console.log(`connected ${connection.threadId}`);
+// exports.create = (req, res) => {
+//   //res.render('add-user')
+// const {firstName, lastName, email,phone} = req.body
+//   pool.getConnection((err, connection) => {
+//     if (err) throw err
+//     console.log(`connected ${connection.threadId}`);
 
-    let searchTerm = req.body.search;
+//     let searchTerm = req.body.search;
 
-    connection.query('INSERT INTO user SET firstName=?, lastName=?,email=?,phone=?',
-      [firstName, lastName, email, phone], (err) => {
-      connection.release();
-      if (!err) {
-        res.render('add-user',)
-      }else{console.log(err);}
-    })
+//     connection.query('INSERT INTO user SET firstName=?, lastName=?,email=?,phone=?',
+//       [firstName, lastName, email, phone], (err) => {
+//       connection.release();
+//       if (!err) {
+//         res.render('add-user',)
+//       }else{console.log(err);}
+//     })
 
-  })
-}
+//   })
+// }
 
-exports.crmentor = (req, res) => {
+// exports.crmentor = (req, res) => {
   
-const {firstName, lastName, Email,PhoneNumber, Role} = req.body
-  pool.getConnection((err, connection) => {
-    if (err) throw err
-    console.log(`connected ${connection.threadId}`);
+// const {firstName, lastName, Email,PhoneNumber, Role} = req.body
+//   pool.getConnection((err, connection) => {
+//     if (err) throw err
+//     console.log(`connected ${connection.threadId}`);
 
-    let Term = req.body.search;
+//     let Term = req.body.search;
 
-    connection.query('INSERT INTO mentor SET firstName=?, lastName=?,Email=?,PhoneNumber=?, Role=?',
-      [firstName, lastName, Email, PhoneNumber, Role], (err) => {
-      connection.release();
-      if (!err) {
-        res.render('mentor',)
-      }else{console.log(err);}
-    })
+//     connection.query('INSERT INTO mentor SET firstName=?, lastName=?,Email=?,PhoneNumber=?, Role=?',
+//       [firstName, lastName, Email, PhoneNumber, Role], (err) => {
+//       connection.release();
+//       if (!err) {
+//         res.render('mentor',)
+//       }else{console.log(err);}
+//     })
 
-  })
-}
+//   })
+// }
 
 
 
